b64a2087801fa29a6c336fc9f3a1ea58e3dc5c44
sever-tron <a.sever778877@gmail.com>
2023-12-11 01:49:30 +0300

    last

--- Beer Code/Assets/Scripts/PlayerController.cs
+++ Beer Code/Assets/Scripts/PlayerController.cs
@@ -1,46 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
-public class PlayerController : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class PlayerController: MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 5f;
-    private Rigidbody rb;
-    private bool isGrounded;
+    public float Speed = 0.3f;
+    public float JumpForce = 1f;
+    public float sensitivity = 5f;
+    public float headMinY = -40f;
+    public float headMaxY = 40f;
+    public Transform head;
+    private float rotationY;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    public LayerMask GroundLayer = 2; 
 
-    void Update()
+    private Rigidbody _rb;
+    private CapsuleCollider _collider;
+    private bool _isGrounded
     {
-        Move();
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        get
         {
-            Jump();
+            var bottomCenterPoint = new Vector3(_collider.bounds.center.x, _collider.bounds.min.y, _collider.bounds.center.z);
+
+
+            return Physics.CheckCapsule(_collider.bounds.center, bottomCenterPoint, _collider.bounds.size.x / 2 * 0.9f, GroundLayer);
+ 
         }
     }
 
-    void Move()
+    private Vector3 _movementVector
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        transform.Translate(movement * moveSpeed * Time.deltaTime);
-    }
-
-    void Jump()
-    {
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        isGrounded = false;
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
+        get
         {
-            isGrounded = true;
+            var horizontal = Input.GetAxis("Horizontal");
+            var vertical = Input.GetAxis("Vertical");
+
+            return new Vector3(horizontal, 0.0f, vertical);
+        }
+    }
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _collider = GetComponent<CapsuleCollider>();
+
+        _rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+
+        if (GroundLayer == gameObject.layer)
+            Debug.LogError("Player SortingLayer must be different from Ground SourtingLayer!");
+    }
+void Update()
+{
+    float rotationX = head.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+    rotationY += Input.GetAxis("Mouse Y") * sensitivity;
+    rotationY = Mathf.Clamp(rotationY, headMinY, headMaxY);
+    head.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+}
+
+void FixedUpdate()
+    {
+        JumpLogic();
+        MoveLogic();
+    }
+
+    private void MoveLogic()
+    {
+        _rb.AddForce(_movementVector * Speed, ForceMode.Impulse);
+    }
+
+    private void JumpLogic()
+    {
+        if (_isGrounded && (Input.GetAxis("Jump") > 0))
+        {
+            _rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
         }
     }
 }
